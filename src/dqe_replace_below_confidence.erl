ee0518c0232db25b650e61ccb3e91c4fe46a9049
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2018-02-28 20:19:29 +0530

    move to custom mmath and fix lint

--- src/dqe_replace_below_confidence.erl
+++ src/dqe_replace_below_confidence.erl
@@ -21,7 +21,8 @@ resolution(_Resolution, State) ->
     {1, State}.
 
 describe(#state{threshold = Threshold, default_value = DefaultValue}) ->
-    ["replace_below_confidence(", float_to_list(Threshold), ", ", float_to_list(DefaultValue), ")"].
+    ["replace_below_confidence(", float_to_list(Threshold), ", ",
+     float_to_list(DefaultValue), ")"].
 
 spec() ->
     {<<"replace_below_confidence">>, [metric, float, float], none, metric}.
@@ -30,5 +31,6 @@ run([Data], S = #state{threshold = Threshold, default_value = DefaultValue}) ->
     {mmath_trans:replace_below_confidence(Data, Threshold, DefaultValue), S}.
 
 help() ->
-    <<"Replaces the metric equal to or below given threshold with the default value."
-      "This can be either applied to raw serieses or aggregates.">>.
+    <<"Replaces the metric equal to or below given threshold with the"
+      " default value. This can be either applied to raw serieses or"
+      " aggregates.">>.
